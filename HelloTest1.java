b34f6b7f2cc524700c54066528a393c8ab333e6f
Balakrishna Sankar <67531913+balakrishnasankar@users.noreply.github.com>
2023-06-20 14:41:11 +0530

    Update HelloTest1.java

--- HelloTest1.java
+++ HelloTest1.java
@@ -4,8 +4,9 @@ public class HelloTest1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-System.out.print("Hello My 1st Jenkins Job Test");
-		System.out.print("Hello My 1st Jenkins Job Test 2");
+System.out.println("Hello My 1st Jenkins Job Test");
+		System.out.println("Hello My 1st Jenkins Job Test 2");
+		System.out.println("Hello My 1st Jenkins Job Test 3");
 	}
 
 }
